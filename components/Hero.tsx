b5587921c908fcec49b5f31f7da461d1b0f883a8
sumit-ganit <sumit.mittal@ganitinc.com>
2025-04-12 15:05:12 +0530

    [fix] line spacing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,8 +20,8 @@ function Hero() {
   className={cn(
     "absolute inset-0",
     "[background-size:40px_40px]",
-    "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-    "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+    "[background-image:linear-gradient(to_right,#e4e4e7_0.1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_0.1px,transparent_1px)]",
+    "dark:[background-image:linear-gradient(to_right,#262626_0.1px,transparent_1px),linear-gradient(to_bottom,#262626_0.1px,transparent_1px)]",
   )}
 />
 </div>
